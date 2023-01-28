fdc0eb99ef66fb7e40482d8f93161c88b3dea9b9
Hwang Seok O <ohs6006@gmail.com>
2023-01-28 18:26:48 +0900

    new Profile

--- src/App.js
+++ src/App.js
@@ -4,7 +4,27 @@ import Profile from "./components/Profile";
 function App() {
   return (
     <div className="App">
-      <Profile />
+      <Profile
+        name="James Kim"
+        jobs="Front End"
+        isNew={true}
+        src="https://images.unsplash.com/photo-1674833482535-e1fcef823bfe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+      />
+      <Profile
+        name="Julia"
+        jobs="Back end"
+        src="https://images.unsplash.com/photo-1674833482535-e1fcef823bfe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+      />
+      <Profile
+        name=""
+        jobs=""
+        src="https://images.unsplash.com/photo-1674833482535-e1fcef823bfe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+      />
+      <Profile
+        name=""
+        jobs=""
+        src="https://images.unsplash.com/photo-1674833482535-e1fcef823bfe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+      />
     </div>
   );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,15 +1,13 @@
 import React from "react";
+import { New } from "./New";
 
-const Profile = () => {
+const Profile = ({ name = "", jobs = "", src = "", isNew = false }) => {
   return (
     <div className="profile">
-      <img
-        className="photo"
-        src="https://images.unsplash.com/photo-1661961110218-35af7210f803?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        alt="pic"
-      />
-      <h1>James Kim</h1>
-      <p>프론트엔드 개발자</p>
+      <img className="photo" src={src} alt="pic" />
+      <h1>{name}</h1>
+      <p>{jobs}</p>
+      {isNew && <New />}
     </div>
   );
 };
