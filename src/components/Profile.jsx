7fc41f865b7f7134571fd9d611df380c311100e2
Hwang Seok O <ohs6006@gmail.com>
2023-01-28 15:12:40 +0900

    Make Profile Components

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="profile">
+      <img
+        className="photo"
+        src="https://images.unsplash.com/photo-1661961110218-35af7210f803?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+        alt="pic"
+      />
+      <h1>James Kim</h1>
+      <p>프론트엔드 개발자</p>
+    </div>
+  );
 };
 
 export default Profile;
